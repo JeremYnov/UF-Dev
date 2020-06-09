6960a5bdfe57e8863f5974007c83468931ceb6a2
Léo Peyré <leo.peyre95@gmail.com>
2020-06-09 15:23:07 +0200

    Class player et game sans repartition du code

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import pygame
 
 pygame.init()
 
+# Creation d'une classe game
+class Game:
+
+    def __init__(self):
+        # generer notre hero
+        self.player = Player()
+
+# creation du hero
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load('assets/player.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 500
+
 # Génération de notre fenêtre de jeu 
 pygame.display.set_caption("UF DEV")
 screen = pygame.display.set_mode((720,920))
@@ -10,12 +31,19 @@ screen = pygame.display.set_mode((720,920))
 # Import du background de l'appli 
 background = pygame.image.load('assets/bg.jpg')
 
+# Charger le jeu
+game = Game()
+
+
 running = True 
 
 # Boucle qui nous permet de garder le jeu allumé 
 while running:
     # Appliquer le background de l'appli 
     screen.blit(background,(0,0))
+
+    #Aplliquer l'image du hero
+    screen.blit(game.player.image, game.player.rect)
 
     # Mettre à jour la fenêtre
     pygame.display.flip()
